0777f4a7bc4e852c32c3e8b5caca12607595aefb
leehs03 <daumnet0731@gmail.com>
2026-03-25 11:02:02 +0900

    Project

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import reactLogo from './assets/react.svg'
+
+function Profile(){
+  return(
+    <>
+    </>
+  )
+}
+
+
+export default function App() {
+  return (
+    <>
+      <img className="button-icon" src={reactLogo} alt="" />          
+    </>
+  )
+}
+
+
